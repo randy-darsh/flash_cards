a88947cbd9885a2d8e97fda2341c83f8bfdc78c4
rspringer47 <rspringer47@gmail.com>
2017-06-14 22:51:52 -0600

    iteration 3 passed

--- lib/guess.rb
+++ lib/guess.rb
@@ -27,3 +27,17 @@ class Guess
   end
 
 end
+
+class Deck
+
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @count = cards.count
+  end
+
+end
--- test/card_test.rb
+++ test/card_test.rb
@@ -30,4 +30,15 @@ class CardTest < Minitest::Test
     assert_equal false, guess.correct?
     assert_equal "Incorrect.", guess.feedback
   end
+
+  def test_it_can_store_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+    deck = Deck.new([card_1, card_2, card_3])
+
+    assert_equal [card_1, card_2, card_3], deck.cards
+    assert_equal 3, deck.count
+  end
+
 end
